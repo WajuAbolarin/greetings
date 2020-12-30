f577b68cc25769fa817562b5b40bae36a1964864
waju <waju.abolarin@gmail.com>
2020-12-30 10:03:14 +0100

    WIP

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,13 @@ const fonts = [
     {name: 'Playfair Display', value: "Playfair Display', serif"},
     {name: 'Roboto', value: "'Roboto', sans-serif"}
 ]
+
+
 export default function IndexPage() {
-    const [text] = useState('<p>This is a sample text</p>')
+    const [text] = useState('')
     const [background, setBackground] = useState(() => Object.keys(backGrounds)[0])
     const [fontFamily, setFontFamily] = useState(fonts[0])
+    const editorRef = useRef()
     const textColor = useMemo(() => {
         return backGrounds[background]
     }, [background])
@@ -29,21 +32,22 @@ export default function IndexPage() {
             e.target.innerText = e.target.innerText.slice(0, 208)
         }
     }, [])
-    const selectRef = useRef()
 
     useEffect(() => {
         document.execCommand("defaultParagraphSeparator", false, "br");
+        const editor = editorRef.current
+        editor.focus()
     }, [])
 
 
     return (
-        <div style={{height: '100vh', background, overflow:'hidden'}}>
+        <div style={{height: '100vh', background, overflow: 'hidden'}} tabIndex="-1">
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link
                 href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Bangers&family=Dancing+Script&family=Indie+Flower&family=Lobster&family=Montserrat:ital,wght@0,300;0,400;1,300;1,400&family=Playfair+Display:ital@0;1&family=Roboto:ital,wght@0,100;0,400;1,300;1,400&display=swap"
                 rel="stylesheet"/>
 
-            <div className="p-5">
+            <div className="p-5" tabIndex="0" style={{zIndex: 3, height: '80px'}}>
                 <p>
                     <select style={{
                         appearance: 'none',
@@ -52,26 +56,29 @@ export default function IndexPage() {
                         borderRadius: '7px',
                         padding: '0.5em',
 
-                    }} ref={selectRef} value={background} onChange={e => setBackground(e.target.value)}>
+                    }}  value={background} onChange={e => setBackground(e.target.value)}>
                         {Object.keys(backGrounds).map(bg => <option value={bg} key={bg}>Background :{bg}</option>)}
                     </select>
 
                     <select style={{
-                        display:'inline-block',
-                        marginLeft:'2em',
+                        display: 'inline-block',
+                        marginLeft: '2em',
                         appearance: 'none',
                         background: 'transparent',
                         border: `1px solid wheat`,
                         borderRadius: '7px',
                         padding: '0.5em',
-                        fontFamily:fontFamily.value
-                    }} ref={selectRef} value={background} onChange={e => setFontFamily(fonts[e.target.value])}>
-                        {fonts.map(( font, index ) => <option style={{fontFamily: font.value}} value={index} key={font.name}>Font: {font.name}</option>)}
+                        fontFamily: fontFamily.value
+                    }}  value={background} onChange={e => setFontFamily(fonts[e.target.value])}>
+                        {fonts.map((font, index) => <option style={{fontFamily: font.value}} value={index}
+                                                            key={font.name}>Font: {font.name}</option>)}
                     </select>
                 </p>
             </div>
 
-            <div contentEditable dangerouslySetInnerHTML={{__html: text.slice(0, 208)}}
+
+            <div ref={editorRef}  contentEditable dangerouslySetInnerHTML={{__html: text.slice(0, 208)}}
+                 data-placeholder="Type a message to Waju"
                  style={{
                      height: '80%',
                      display: 'grid',
@@ -82,12 +89,9 @@ export default function IndexPage() {
                      wordBreak: 'break-word',
                      lineHeight: 1.6,
                      outline: 'none',
-                     fontFamily:fontFamily.value
+                     fontFamily: fontFamily.value
                  }}
-                 onClick={() => {
-                     selectRef.current.focus()
-                 }}
-                 className={`text-3xl md:text-7xl w-100 p-10 text-${textColor}`}
+                 className={`text-3xl md:text-7xl w-100 p-10 text-${textColor} message-editor`}
                  onInput={handleChange}
             />
         </div>
